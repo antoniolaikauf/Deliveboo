aba22c566deb2a8ee3a3a7909bf183dc48ab3e93
Fonta97 <fontana.alessandro97@gmail.com>
2024-03-11 14:23:17 +0100

    Restaurant inizio

--- laravel/front-end/src/pages/AppRestaurant.vue
+++ laravel/front-end/src/pages/AppRestaurant.vue
@@ -5,13 +5,33 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div>
-            <router-link :to="{ name: 'success' }"> sono success </router-link>
+    <!-- sezioni ristoranti impostazione -->
+    <section>
+        <div class="restaurantpic d-flex p-4">
+                <img class="img-fluid" src="https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="placeholderrestaurant">
+                <div class="p-2"><h1 class="restaurantName">NomeRistorante</h1></div>
         </div>
-    </div>
+    </section>
+
+    <!-- barra checkout impostazione -->
+    <section class="checkoutbar d-flex justify-content-center">
+            <router-link :to="{ name: 'success' }"> <button class="btn-boo">Checkout</button></router-link>
+    </section>
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+@use '../styles/general.scss' as *;
+
+.restaurantpic{
+    max-width: 500px;
+    max-height: 500px;
+}
+
+.restaurantName {
+    font-family: $boo-font;
+    font-weight: bolder;
+}
 
 </style>
