8700b786984ec95fec87b1ebd75bac9081e3acfa
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-03-11 13:00:59 +0100

    feat: jumbotron & fix header

--- laravel/front-end/src/components/ComponentHeader.vue
+++ laravel/front-end/src/components/ComponentHeader.vue
@@ -14,11 +14,22 @@ export default {
                     alt="logo-deliveboo"
                 />
 
+                <div class="search-container">
+                    <i class="fa-solid fa-paper-plane"></i>
+
+                    <input class="search" type="text" placeholder="Ricerca">
+                    <button class="btn  input-btn">
+                        Cerca
+                    </button>
+                </div>
+
+
+
                 <div class="d-flex">
                     <!-- BOTTONE LOGIN -->
 
                     <router-link :to="{ name: 'Login' }">
-                        <button class="d-flex btn-boo mx-3">
+                        <button class="d-none d-md-flex  btn-boo mx-3">
                             <div class="mx-3">
                                 <i class="fa-solid fa-house color"></i>
                             </div>
@@ -29,9 +40,18 @@ export default {
                         </button>
                     </router-link>
 
+                    <!-- BOTTONE LOGIN (RESPONSIVE) -->
+                    <router-link :to="{ name: 'Login' }">
+                        <button class=" btn-boo d-md-none mx-3">
+                            <div class="mx-3">
+                                <i class="fa-solid fa-house color"></i>
+                            </div>
+                        </button>
+                    </router-link>
+
                     <!-- BOTTONE ADVANCE (ricerca) -->
                     <button
-                    class="d-flex btn-boo"
+                    class="d-none d-md-flex  btn-boo"
                         type="button"
                         data-bs-toggle="offcanvas"
                         data-bs-target="#offcanvasRight"
@@ -44,6 +64,20 @@ export default {
                         <div>
                             <span>Advance</span>
                         </div>
+                    </button>
+
+                    <!-- BOTTONE ADVANCE (RESPONSIVE) -->
+                    <button
+                    class="d-flex btn-boo d-md-none"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasRight"
+                        aria-controls="offcanvasRight"
+                    >
+                        <div class="mx-3">
+                            <i class="fa-solid fa-utensils color"></i>
+                        </div>
+
                     </button>
 
                     <div
@@ -90,7 +124,7 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-*
+@use '../styles/partials/variables' as *;
 .logo {
     width: 100px;
 }
@@ -103,4 +137,57 @@ a {
     border: 1px solid rgb(227, 221, 221);
 }
 
+.search-container {
+    position: relative;
+}
+
+.search {
+    width: 400px;
+    height: 50px;
+    border-radius: 30px;
+    border: 1px solid $boo-text-color;
+    padding-left: 40px;
+}
+
+
+.input-btn {
+    position: absolute;
+    top: 6px;
+    right: 9px;
+    height: 75%;
+    margin: 0;
+    padding: 0px 15px;
+    border-radius: 24px;
+    color: white;
+    background-color: $boo-text-color;
+}
+
+
+.fa-paper-plane {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    color: $boo-text-color;
+    transform: translateY(-50%);
+}
+
+@media screen and (max-width: 850px) {
+    .search {
+        width: 300px;
+    }
+}
+
+@media screen and (max-width: 650px) {
+    .search {
+        width: 250px;
+    }
+
+}
+
+@media screen and (max-width: 590px) {
+    .search {
+        width: 180px;
+    }
+
+}
 </style>
