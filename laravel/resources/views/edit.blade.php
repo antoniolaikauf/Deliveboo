025722e6810b2876a68bb35e3253528e08ee0b28
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-03-15 00:13:30 +0100

    add: aggiunto select in edit

--- laravel/resources/views/edit.blade.php
+++ laravel/resources/views/edit.blade.php
@@ -1,42 +1,53 @@
 @extends('layouts.app')
 @section('content')
-    <form action="{{ route('dish.update', $dish->id) }}" method="POST">
+<form action="{{ route('dish.update', $dish->id) }}" method="POST">
 
-        @csrf
-        @method('PUT')
+    @csrf
+    @method('PUT')
 
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 
-        <div>
-            <label for="name">Nome del piatto</label>
-            <input type="text" name="name" id="name" value="{{ $dish->name }}">
-        </div>
+    <div>
+        <label for="name">Nome del piatto</label>
+        <input type="text" name="name" id="name" value="{{ $dish->name }}">
+    </div>
 
-        <div>
-            <label for="description">Descrizione delp piatto</label>
-            <textarea name="description" id="description" cols="30" rows="10">{{ $dish->description }}</textarea>
-        </div>
+    <div>
+        <label for="description">Descrizione delp piatto</label>
+        <textarea name="description" id="description" cols="30" rows="10">{{ $dish->description }}</textarea>
+    </div>
 
-        <div>
-            <label for="price">Prezzo del piatto</label>
-            <input type="number" name="price" id="price" value="{{ $dish->price }}">
-        </div>
+    <div>
+        <label for="price">Prezzo del piatto</label>
+        <input type="number" name="price" id="price" value="{{ $dish->price }}">
+    </div>
 
-        <div>
+    <!-- <div>
             <label for="available">Disponibilità</label>
             <input type="text" name="available" id="available" placeholder="Inserisci 1 per SI e 0 per NO"
                 value="{{ $dish->available }}">
-        </div>
+        </div> -->
+    <div>
+        <select name="available" id="available">
+            <option value="1" @if( $dish->available===1)
+                selected
+                @endif
+                >disponibile</option>
+            <option value="0" @if( $dish->available===0)
+                selected
+                @endif>non disponibile</option>
+        </select>
+    </div>
 
-        <input type="submit" value="MODIFICA">
+    <input type="submit" value="MODIFICA">
 
-    </form>
+</form>
 @endsection
