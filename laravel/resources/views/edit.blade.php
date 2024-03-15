6975b0865d7ff72f007543650886a37e9fed6ba3
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-03-15 10:15:18 +0100

    fix: edit

--- laravel/resources/views/edit.blade.php
+++ laravel/resources/views/edit.blade.php
@@ -1,53 +1,68 @@
 @extends('layouts.app')
 @section('content')
-<form action="{{ route('dish.update', $dish->id) }}" method="POST">
 
-    @csrf
-    @method('PUT')
+    <h2 class="text-center mt-3">Modifica il tuo piatto</h2>
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+    <form action="{{ route('dish.update', $dish->id) }}" method="POST">
 
-    <div>
-        <label for="name">Nome del piatto</label>
-        <input type="text" name="name" id="name" value="{{ $dish->name }}">
-    </div>
+        @csrf
+        @method('PUT')
 
-    <div>
-        <label for="description">Descrizione delp piatto</label>
-        <textarea name="description" id="description" cols="30" rows="10">{{ $dish->description }}</textarea>
-    </div>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-    <div>
-        <label for="price">Prezzo del piatto</label>
-        <input type="number" name="price" id="price" value="{{ $dish->price }}">
-    </div>
+        <div class="d-flex mb-2 align-items-center">
+            <label for="name">Nome del piatto</label>
+            <input type="text" class="form-control" name="name" id="name" value="{{ $dish->name }}">
+        </div>
 
-    <!-- <div>
-            <label for="available">Disponibilità</label>
-            <input type="text" name="available" id="available" placeholder="Inserisci 1 per SI e 0 per NO"
-                value="{{ $dish->available }}">
-        </div> -->
-    <div>
-        <select name="available" id="available">
-            <option value="1" @if( $dish->available==1)
-                selected
-                @endif
-                >disponibile</option>
-            <option value="0" @if( $dish->available==0)
-                selected
-                @endif>non disponibile</option>
-        </select>
-    </div>
+        <div class="d-flex mb-2 align-items-start">
+            <label for="description">Descrizione delp piatto</label>
+            <textarea name="description" class="form-control" id="description" cols="30" rows="10">{{ $dish->description }}</textarea>
+        </div>
 
-    <input type="submit" value="MODIFICA">
+        <div class="d-flex mb-2 align-items-center">
+            <label for="price">Prezzo del piatto</label>
+            <input type="number" class="form-control" name="price" id="price" value="{{ $dish->price }}">
+        </div>
 
-</form>
+        <!-- <div class="d-flex mb-2 align-items-center">
+                                                                                                                <label for="available">Disponibilità</label>
+                                                                                                                <input type="text" name="available" id="available" placeholder="Inserisci 1 per SI e 0 per NO"
+                                                                                                                    value="{{ $dish->available }}">
+                                                                                                            </div> -->
+        <div class="d-flex mb-3 align-items-center">
+            <label for="available">disponibile</label>
+            <select name="available" id="available" class="form-control">
+                <option value="1" @if ($dish->available == 1) selected @endif>disponibile</option>
+                <option value="0" @if ($dish->available == 0) selected @endif>non disponibile</option>
+            </select>
+        </div>
+
+        <div class="d-flex mb-2 justify-content-center">
+
+            <input type="submit" value="MODIFICA">
+
+        </div>
+
+
+
+    </form>
+    <style>
+        form {
+            width: 40%;
+            margin: 50px auto;
+        }
+
+        label {
+            width: 250px;
+        }
+    </style>
 @endsection
