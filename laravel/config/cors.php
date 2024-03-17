517bcff8f2e4636200983233ae0629103ae36915
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-03-17 08:10:41 +0100

    fix: sistemato porta localhost

--- laravel/config/cors.php
+++ laravel/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['POST', 'GET', 'OPTIONS'],
 
-    'allowed_origins' => ['http://localhost:5174'],
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
