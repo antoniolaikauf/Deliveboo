cf11c8a07362a97c7e1438f6fa826ed0c7026d28
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-03-18 15:17:26 +0100

    try

--- laravel/front-end/src/components/ComponentJambo.vue
+++ laravel/front-end/src/components/ComponentJambo.vue
@@ -7,15 +7,41 @@ export default {
 <template>
     <div class="container-fluid gx-0">
         <div class="jumbotron">
+<<<<<<< HEAD
             <div class="description text-white">
                 <h1>I piatti dei ristoranti che ami <br> e la spesa, a domicilio </h1>
                 <p>Inserisci un indirizzo nell'input in alto per scoprire <br> le opzioni disponibili nella tua zona</p>
+=======
+            <video autoplay loop muted playsinline class="background-clip">
+                <source src="/public/preparingdeliveboo.mp4" type="video/mp4" />
+                <!-- cidooododdood -->
+            </video>
+            <div class="description text-white">
+                <h1>
+                    I piatti dei ristoranti che ami <br />
+                    e la spesa, a domicilio
+                </h1>
+                <p style="font-size: 25px">
+                    Esplora una vasta varietà di cucine dai migliori ristoranti
+                    della tua zona, <br />
+                    e goditi il piacere della consegna a domicilio direttamente
+                    sul tuo tavolo!
+                </p>
+>>>>>>> 0e91c32 (try)
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+<<<<<<< HEAD
+=======
+.jumbotron {
+    position: relative;
+    height: 450px;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+>>>>>>> 0e91c32 (try)
 
 .jumbotron {
     background-image: url(/public/jumbotron.jpg);
