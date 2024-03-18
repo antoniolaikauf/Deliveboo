4b301e054ab93bc09e5434cd090aca3feb4d8990
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2024-03-18 17:03:52 +0100

    fix: dasboard

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -29,44 +29,44 @@
         </video>
         <div class="card text-center py-4">
             @foreach ($restaurants as $restaurant)
-            @if (Auth::check() && Auth::id() === $restaurant->user_id)
-            <h1 class="mb-4">{{ $restaurant->name }}</h1>
-            <div class="img">
-                <img src="{{ asset('storage/' . $restaurant->img) }}" alt="{{ $restaurant->name }}">
-            </div>
-            @endif
+                @if (Auth::check() && Auth::id() === $restaurant->user_id)
+                    <h1 class="mb-4">{{ $restaurant->name }}</h1>
+                    <div class="img">
+                        <img src="{{ asset('storage/' . $restaurant->img) }}" alt="{{ $restaurant->name }}">
+                    </div>
+                @endif
             @endforeach
-            <div>
+            <div class="mt-5">
                 <a href="http://localhost:8000" class="my-4 link-dashboard">Vai al tuo menù</a>
             </div>
         </div>
     </div>
-<style>
-    .img img {
-        width: 100%;
-        max-height: 500px;
-        object-fit: cover;
-    }
+    <style>
+        .img img {
+            width: 100%;
+            max-height: 500px;
+            object-fit: cover;
+        }
 
-    .link-dashboard {
-        padding: 7px;
-        border: 1px solid black;
-        border-radius: 5px;
-        width: 200px;
-    }
+        .link-dashboard {
+            padding: 7px;
+            border: 1px solid black;
+            border-radius: 5px;
+            width: 200px;
+        }
 
-    .link-dashboard:hover {
-        border: 1px solid #22cdd0;
-    }
+        .link-dashboard:hover {
+            border: 1px solid #22cdd0;
+        }
 
-    .background-clip {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        object-fit: cover;
-        z-index: -1;
-        height: 100vh
-    }
-</style>
+        .background-clip {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            object-fit: cover;
+            z-index: -1;
+            height: 100vh
+        }
+    </style>
 @endsection
