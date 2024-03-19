47be318726dfb9953f468c15f20b94e1d28c825b
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-03-19 12:54:54 +0100

    config: table

--- laravel/resources/views/dish.blade.php
+++ laravel/resources/views/dish.blade.php
@@ -1,180 +1,131 @@
 @extends('layouts.app')
+
 @section('content')
-    <div class="container-fluid bg-img">
+<div class="container-fluid bg-img">
+    <div class="container">
         <div class="row">
             <div class="col-12">
                 @auth
-                    <div class="text-center m-4">
-                        <h2 class="text-white auth m-4">
-                            Ciao, {{ Auth::user()->name }}. Ecco tutti i piatti disponibili nel tuo account.
-                        </h2>
-                        <a href="{{ route('dish.create') }}" class="btn-boo buttons p-2">Crea il piatto</a>
+                <div class="text-center m-4">
+                    <h2 class="text-white auth m-4">
+                        Ciao, {{ Auth::user()->name }}. Ecco tutti i piatti disponibili nel tuo account.
+                    </h2>
+                            <a href="{{ route('dish.create') }}" class="btn-boo buttons bg-black text-white p-2">Crea il piatto</a>
+                        </div>
+                        @endauth
                     </div>
-                @endauth
-            </div>
-            <div class="col-12 d-flex flex-wrap">
-                @foreach ($dishes as $dish)
-                    @if (Auth::check() && Auth::id() === $dish->user_id)
-                        <div class="col-12 col-lg-6 p-4">
-                            <div class="card p-5 card-restaurant">
-
-
-                                <div class="button-dish mt-2">
-                                    <div>
-                                        @auth
-                                            <a href="{{ route('dish.edit', $dish->id) }}" class=" mx-2"><button class="btn-boo buttons"><i class="fa-solid fa-pen-to-square"></i></button></a>
-                                        @endauth
-                                    </div>
-
-                                    <div class="py-2">
-                                        @auth
-                                            <form id="deleteForm_{{ $dish->id }}"
-                                                action="{{ route('dish.delete', $dish->id) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="button" class="btn btn-danger"
-                                                    onclick="showConfirmationModal('{{ $dish->id }}')"><i class="fa-solid fa-trash" style="color: white"></i></i></button>
-                                            </form>
-                                        @endauth
-                                    </div>
-                                </div>
-
-                                <div class="card-body text-center ">
-                                    <h5 class="card-title">Nome del piatto: <strong>{{ $dish->name }}</strong></h5>
-                                    <p class="card-text">{{ $dish->description }}</p>
-                                    <div class="my-2">Prezzo: <strong>{{ $dish->price }}&#128;</strong></div>
-
-                                    <div class="dish-availability">
-                                        <div>
-                                            @if ($dish->available)
-                                                <div>
-                                                    <span class="availability-indicator available">Piatto disponibile</span>
-                                                </div>
-                                            @else
-                                                <div>
-                                                    <span class="availability-indicator unavailable">Piatto non disponibile</span>
-                                                </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Descrizione</th>
+                                    <th scope="col" style="width: 100px;">Prezzo</th>
+                                    <th scope="col">Disponibilità</th>
+                                    <th scope="col">Immagini</th>
+                                    <th scope="col" style="width: 90px;">Azioni</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($dishes as $dish)
+                                @if (Auth::check() && Auth::id() === $dish->user_id)
+                                <tr>
+                                    <td>{{ $dish->name }}</td>
+                                    <td>{{ $dish->description }}</td>
+                                    <td>{{ $dish->price }} €</td>
+                                    <td>
+                                        @if ($dish->available)
+                                        <span class="badge bg-success">Disponibile</span>
+                                        @else
+                                        <span class="badge bg-danger">Non disponibile</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <div class="img-cont">
+                                            @if($dish->img)
+                                            <img class="img-fluid" src="{{asset('storage/'.$dish->img)}}" alt="Immagine del piatto {{ $dish->name }}">
                                             @endif
                                         </div>
                                     </div>
-
-                                    <div class="img_container ">
-                                        @if($dish->img)
-                                            <img class="img-fluid" src="{{asset('storage/'.$dish->img)}}" alt="Immagine del piatto {{ $dish->name }}">
-                                        @endif
-
-
-                                    </div>
-
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Modal -->
-                        <div class="modal fade" id="confirmationModal_{{ $dish->id }}" tabindex="-1"
-                            aria-labelledby="confirmationModalLabel_{{ $dish->id }}" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="confirmationModalLabel_{{ $dish->id }}">Attenzione <i class="fa-solid fa-bomb"></i></h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <h6>Sei sicuro di voler eliminare "{{ $dish->name }}" ?</h6>
-                                    </div>
-                                    <div class="modal-footer">
-                                        {{-- <button type="button" class="btn btn-secondary"
-                                            data-bs-dismiss="modal">Cancella</button> --}}
-                                        <button type="button" class="btn btn-danger"
-                                            onclick="submitDeleteForm('{{ $dish->id }}')">Elimina</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
+                                </td>
+                                <td>
+                                    @auth
+                                    <a href="{{ route('dish.edit', $dish->id) }}" class="btn btn-sm btn-primary">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <button class="btn btn-sm btn-danger" onclick="showConfirmationModal('{{ $dish->id }}')">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                    <form id="deleteForm_{{ $dish->id }}"
+                                        action="{{ route('dish.delete', $dish->id) }}" method="POST" style="display: none;">
+                                        @csrf
+                                        @method('DELETE')
+                                    </form>
+                                    @endauth
+                                </td>
+                            </tr>
+                            @endif
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
-    <style>
 
-        /* stile disponibilità */
-        .dish-availability {
-            margin-top: 10px;
-        }
+        @foreach ($dishes as $dish)
+            @if (Auth::check() && Auth::id() === $dish->user_id)
+            <div class="modal fade" id="confirmationModal_{{ $dish->id }}" tabindex="-1"
+                aria-labelledby="confirmationModalLabel_{{ $dish->id }}" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="confirmationModalLabel_{{ $dish->id }}">Attenzione</h5>
+                            <i class="fas fa-exclamation-triangle"></i>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h6>Sei sicuro di voler eliminare "{{ $dish->name }}"?</h6>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" onclick="submitDeleteForm('{{ $dish->id }}')">Elimina</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
+        @endforeach
 
-        .availability-indicator {
-            padding: 0.5rem 1rem;
-            border-radius: 20px;
-            font-size: 0.9rem;
-            font-weight: bold;
-        }
+        <script>
+            function showConfirmationModal(dishId) {
+                $('#confirmationModal_' + dishId).modal('show');
+            }
 
-        .available {
-            background-color: #28a745;
-            color: #fff;
-        }
+            function submitDeleteForm(dishId) {
+                document.getElementById('deleteForm_' + dishId).submit();
+            }
+            </script>
 
-        .unavailable {
-            background-color: #dc3545;
-            color: #fff;
-        }
+<style>
+    .img-cont .img-fluid{
+        height: 50px;
+        width: 100%;
+        object-fit: contain;
+    }
 
-        /* stile titolo */
-        .auth {
-            letter-spacing: 3px;
-        }
-
-        /* stile bg */
-        .bg-img {
-            background-image: url(/imgs/8afbf530-609f-4e88-99f1-628c2a9faa63.png);
-            background-size: cover;
-            height: 100%;
+    .bg-img {
+        background-image: url(/imgs/8afbf530-609f-4e88-99f1-628c2a9faa63.png);
+        background-size: cover;
+        height: 100%;
             width: 100%;
         }
 
-        /* stile card */
-        .card-restaurant {
-            border: 1px solid #ddd;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            transition: transform 0.3s ease;
-            height: 500px;
-            position: relative;
-        }
-
-        .card-restaurant:hover {
-            transform: scale(1.05);
-        }
-
-        .img_container img {
-            width: 100%;
-            max-height: 250px;
-            object-fit: cover;
-            margin-top: 35px;
-            height: 300px;
-        }
-
-        .button-dish{
-            position: absolute;
-            top: 0;
-            right: 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
     </style>
 
-    {{-- SCRIPT MODALE --}}
-    <script>
-        function showConfirmationModal(dishId) {
-            $('#confirmationModal_' + dishId).modal('show');
-        }
-
-        function submitDeleteForm(dishId) {
-            document.getElementById('deleteForm_' + dishId).submit();
-        }
-    </script>
 @endsection
+
+
+
