d82008155466dff933d9fd3c0a5cb134493d1d9f
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-03-21 15:53:19 +0100

    add: aggiunto payment

--- laravel/front-end/src/pages/AppSuccess.vue
+++ laravel/front-end/src/pages/AppSuccess.vue
@@ -11,16 +11,49 @@ export default {
         return {
             store,
             token: "",
+            dropInInstance: null,
+            error: "",
         };
     },
+    // computed: {
+    //     calculateTotal() {
+    //         // Implementa il calcolo del totale del carrello
+    //         return this.store.cart.reduce(
+    //             (acc, item) => acc + item.totalPrice,
+    //             0
+    //         );
+    //     },
+    // },
     methods: {
+        submitPayment() {
+            if (!this.dropInInstance) {
+                console.error("Drop-in instance non inizializzata.");
+                return;
+            }
+            this.dropInInstance.requestPaymentMethod((err, payload) => {
+                if (err) {
+                    console.error(
+                        "Errore nella richiesta del metodo di pagamento:",
+                        err
+                    );
+                    this.onError(err);
+                    return;
+                }
+                // Qui invii il payload.nonce al tuo server per processare il pagamento
+                console.log("Nonce ottenuto:", payload.nonce);
+                this.onSuccess(payload);
+            });
+        },
         onSuccess(payload) {
             let nonce = payload.nonce;
-            // Do something great with the nonce...
+            // Implementa cosa fare dopo aver ottenuto il nonce, es. inviare al server per processare il pagamento
+            console.log("Pagamento riuscito con nonce:", nonce);
+            // Puoi qui anche navigare a una pagina di successo o mostrare un messaggio
         },
         onError(error) {
-            let message = error.message;
-            // Whoops, an error has occured while trying to get the nonce
+            const message = error.message;
+            console.error("Errore nel processo di pagamento:", message);
+            console.log(message);
         },
     },
     mounted() {
@@ -46,106 +79,42 @@ export default {
 </script>
 
 <template>
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <h1>{{ store.restaurantselected.name }}</h1>  
-            <h3>Riepilogo Ordine</h3>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Prodotto</th>
-                        <th>Quantità</th>
-                        <th>Prezzo unitario</th>
-                        <th>Totale</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- Inizio dettagli ordine -->
-                    <tr v-for="item in store.cart" >
-                        <td>{{ item.dish.name }}</td>
-                        <td>{{ item.quantity }}</td>
-                        <td>{{ item.dish.price }}</td>
-                        <td>{{ item.totalPrice }}</td>
-                    </tr>
-                    <!-- <tr>
-                        <td>Pomodoro</td>
-                        <td>3</td>
-                        <td>€ 1,00</td>
-                        <td>€ 3,00</td>
-                    </tr> -->
-                    <!-- Fine -->
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="3" class="text-right">Totale</td>
-                        <td>€ 13,00</td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    </div>
-
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <h3>Inserisci i tuoi dati</h3>
-                <form id="orderForm" method="post" action="process_order.php">
-                    <div class="form-group">
-                        <label for="name">Nome</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            name="name"
-                            required
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="email">E-mail</label>
-                        <input
-                            type="email"
-                            class="form-control"
-                            id="email"
-                            name="email"
-                            required
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="address">Indirizzo</label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="address"
-                            name="address"
-                            required
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="phone">Numero di Telefono</label>
-                        <input
-                            type="tel"
-                            class="form-control"
-                            id="phone"
-                            name="phone"
-                            pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        class="btn btn-primary"
-                        id="submit-button"
-                        @click.prevent="processPayment"
-                    >
-                        Paga adesso
-                    </button>
-                </form>
+            <div class="col-md-8 offset-md-2">
+                <h1>{{ store.restaurantselected.name }}</h1>
+                <h3>Riepilogo Ordine</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Prodotto</th>
+                            <th>Quantità</th>
+                            <th>Prezzo unitario</th>
+                            <th>Totale</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in store.cart" :key="item.dish.id">
+                            <td>{{ item.dish.name }}</td>
+                            <td>{{ item.quantity }}</td>
+                            <td>{{ item.dish.price }}</td>
+                            <td>{{ item.totalPrice }}</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="3" class="text-right">Totale</td>
+                            <td>€ {{ calculateTotal }}</td>
+                        </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
-        <div id="dropin-container" class="col-md-6 offset-md-3"></div>
+        <div id="dropin-container" class="mt-5"></div>
+        <button class="btn btn-primary mt-3" @click="submitPayment">
+            Paga adesso
+        </button>
     </div>
-</div>
 </template>
 
 <style lang="scss">
