8259772e3e983012e677d35ecf5d35365b2d4ded
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-03-28 10:31:21 +0100

    fix button not-found

--- laravel/front-end/src/pages/NotFound.vue
+++ laravel/front-end/src/pages/NotFound.vue
@@ -15,6 +15,10 @@
                 </div>
                 <div class="col-12 col-lg-6">
                     <h1>Oops! Sembra che tu abbia preso una direzione sbagliata. <br> Prova a ritornare indietro e navigare nella giusta direzione!</h1>
+
+                    <router-link :to="{ name: 'home' }">
+                        <button class="btn-boo mt-5 mb-4">Ritorna alla Home</button>
+                    </router-link>
                 </div>
             </div>
         </div>
