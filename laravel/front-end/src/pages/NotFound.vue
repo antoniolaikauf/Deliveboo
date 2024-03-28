89fb57dbb37f2cba7e600a279bd145b95119b89c
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-03-28 10:28:36 +0100

    feat: Not Found

---
+++ laravel/front-end/src/pages/NotFound.vue
@@ -0,0 +1,34 @@
+
+<script>
+    export default {
+    name: "NotFound",
+
+}
+</script>
+
+<template>
+    <div class="container-fluid bg-not-found">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-6">
+                    <img src="/public/DelivebooNoBG.svg" alt="logo deliveboo">
+                </div>
+                <div class="col-12 col-lg-6">
+                    <h1>Oops! Sembra che tu abbia preso una direzione sbagliata. <br> Prova a ritornare indietro e navigare nella giusta direzione!</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../styles/partials/mixins" as *;
+@use "../styles/partials/variables" as *;
+@use "../styles/general.scss" as *;
+
+    .bg-not-found{
+        background-color: $boo-color;
+    }
+
+</style>
